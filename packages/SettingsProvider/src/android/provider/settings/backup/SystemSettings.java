5d2570119f13d48982c4c7310440aa1ee30b2dbf
Michael Bestas <mkbestas@lineageos.org>
2018-01-21 00:22:10 +0200

    frameworks: Add support for advanced restart options
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Dave Kessler <activethrasher00@gmail.com>
    Co-authored-by: DvTonder <david.vantonder@gmail.com>
    Co-authored-by: Jorge Ruesga <jorge@ruesga.com>
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Co-authored-by: Ricardo Cerqueira <cyanogenmod@cerqueira.org>
    Co-authored-by: Roman Birg <roman@cyngn.com>
    Co-authored-by: SagarMakhar <sagarmakhar@gmail.com>
    Co-authored-by: Veeti Paananen <veeti.paananen@rojekti.fi>
    Co-authored-by: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Change-Id: I4b35d10b0811eff0d8e9ab68b2aedbe52b4aa53f

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -128,7 +128,8 @@ public class SystemSettings {
                 Settings.System.CV_ENABLED,
                 Settings.System.RINGTONE_VIBRATION_PATTERN,
                 Settings.System.NOTIFICATION_VIBRATION_PATTERN,
-                Settings.System.THREE_FINGER_GESTURE
+                Settings.System.THREE_FINGER_GESTURE,
+                Settings.System.ADVANCED_REBOOT
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);
