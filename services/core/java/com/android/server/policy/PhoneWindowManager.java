898bb6e5c767d7d7bda9a85c1c624cad56a7b2b8
rituj <ritujbeniwal@gmail.com>
2020-02-25 10:13:05 +0530

    KeyHandler: Use pocket judge to update proximity state
    Change-Id: I191faaeab61d170df2b35432cdbe05fadae662ca
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -743,9 +743,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
             if (wasDeviceInPocket != mIsDeviceInPocket) {
                 handleDevicePocketStateChanged();
-                //if (mKeyHandler != null) {
-                    //mKeyHandler.setIsInPocket(mIsDeviceInPocket);
-                //}
+                if (mKeyHandler != null) {
+                    mKeyHandler.setIsInPocket(mIsDeviceInPocket);
+                }
             }
         }
 
