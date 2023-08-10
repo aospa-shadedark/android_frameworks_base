6a834324367d1e9b950c54272b62f0b31e283960
jhenrique09 <jhsv09@gmail.com>
2023-08-10 14:22:06 +0000

    Implement DeviceConfigUtils and service
    - Allow overriding device configs while using GMS as device configurator package
    
    Change-Id: Ie7327610feca7a390915511088dfef831c946853
    
    DeviceConfigUtils: Make debug optional
    
    Change-Id: I65ffac8fd0a9971297b8d721d860face571a2d44
    (cherry picked from commit 9f54af5392d7ca70e1e495fc97560538459844e7)
    
    DeviceConfig: Change setProperties behavior
    
    Change-Id: I7cf1574f423c7362e6a7c9d8a002e4b20e5e7f87
    (cherry picked from commit f94a2c97e7bf53ee6681183f2394878369e99470)
    
    * Adapt to Android 14
    
    Change-Id: I6ece899c13f12ee299ba24b02f2b4ab96f3d92fb
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -174,6 +174,7 @@ import com.android.server.contentsuggestions.ContentSuggestionsManagerService;
 import com.android.server.contextualsearch.ContextualSearchManagerService;
 import com.android.server.coverage.CoverageService;
 import com.android.server.cpu.CpuMonitorService;
+import com.android.server.crdroid.CustomDeviceConfigService;
 import com.android.server.credentials.CredentialManagerService;
 import com.android.server.criticalevents.CriticalEventLog;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
@@ -3018,6 +3019,10 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartHealthService");
             mSystemServiceManager.startService(HealthInterfaceService.class);
+            t.traceEnd();
+
+            t.traceBegin("StartCustomDeviceConfigService");
+            mSystemServiceManager.startService(CustomDeviceConfigService.class);
             t.traceEnd();
         }
 
