b3379e823491bc339dbcf3acd1e35732328eeef7
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-03 18:29:50 +0800

    Hide App list: Improve hide app list feature
    Insprired by: https://github.com/j-hc/zygisk-detach, instead of hooking libbinder, we override the computerengine to hide the app to the caller package. we should hide apps whereever possible except for system apps like launcher,settings, and framework. The main goal of Hide app list is to hide apps from playstore and banking apps, however we don't want to break package installer and system app list.
    
    Change-Id: I6d6cb159bde5dbabf37f313363996943ac881623
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -2915,6 +2915,8 @@ public final class SystemServer implements Dumpable {
             t.traceBegin("StartHealthService");
             mSystemServiceManager.startService(HealthInterfaceService.class);
             t.traceEnd();
+            
+            mSystemServiceManager.startService(HideAppListService.class);
         }
 
         t.traceBegin("StartMediaProjectionManager");
