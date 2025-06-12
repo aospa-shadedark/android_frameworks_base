a989f0793338c88c98e969b8e8ded98c43dc93c3
songlongdaiduong <song.long.dai.duong@gmail.com>
2025-06-12 17:20:41 +0800

    base: Allow user to provide pif data [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12722,6 +12722,13 @@ public final class Settings {
         public static final String HIDE_APPLIST = "hide_applist";
 
         /**
+         * User selectable pif data.
+         * @hide
+         */
+        @Readable
+        public static final String PIF_DATA = "pif_data";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
