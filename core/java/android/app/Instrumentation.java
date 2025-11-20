8e2190ae0922d63222e72d8ced85bd5b240d12ce
kenway214 <kenway214@outlook.com>
2025-11-21 02:21:20 +0545

    base: Introduce per-apps spoofing [1/2]
    Change-Id: Ib30911d0f9d68630b8c833923dd6158ceaca1fad
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -63,6 +63,7 @@ import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
 import com.android.internal.util.PropImitationHooks;
+import com.android.internal.util.neoteric.GamesPropsUtils;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -72,6 +73,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.StringJoiner;
 import java.util.concurrent.TimeoutException;
+
 
 /**
  * Base class for implementing application instrumentation code.  When running
@@ -1354,6 +1356,7 @@ public class Instrumentation {
                 .instantiateApplication(cl, className);
         app.attach(context);
         PropImitationHooks.setProps(context);
+        GamesPropsUtils.setProps(context);
         return app;
     }
     
@@ -1372,6 +1375,7 @@ public class Instrumentation {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
         PropImitationHooks.setProps(context);
+        GamesPropsUtils.setProps(context);
         return app;
     }
 
